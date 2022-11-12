621e7489656a67cbebb9808f6d4356d69f342ac3
Sanju19041994 <Sanjuchakole.sanju@gmail.com>
2022-11-12 14:10:56 +0530

    FIR-1 : create project in spring boot

---
+++ src/main/java/com/bIt/JiraGitApplication.java
@@ -0,0 +1,13 @@
+package com.bIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bIt/JiraGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
